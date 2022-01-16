b59f297fd1f1b53fc6e721f7c2942e4fe7f59663
Wojtek <wojciech.cz7@gmail.com>
2022-01-16 19:29:17 +0100

    Logging out of the IdP

--- src/ImageGallery.Client/Controllers/GalleryController.cs
+++ src/ImageGallery.Client/Controllers/GalleryController.cs
@@ -2,6 +2,7 @@
 using ImageGallery.Model;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
@@ -184,6 +185,7 @@ namespace ImageGallery.Client.Controllers
         public async Task Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme); // DZIEKI TEMU TEŻ WYLOGUJE Z IdP !!
         }
 
         public async Task WriteOutIdentityInformation()
